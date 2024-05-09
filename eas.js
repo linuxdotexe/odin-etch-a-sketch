181fc738a2bf9ed9c610394200eefb2faf9f70c0
sai nivas mangu <sainonbeat99@gmail.com>
2024-05-09 13:57:36 +0530

    add: better prompt

--- eas.js
+++ eas.js
@@ -1,6 +1,16 @@
 const container = document.querySelector("#container");
 const allRows = document.getElementsByClassName("row");
-const GRID_SIZE = parseInt(prompt("Enter grid size:", "16")) || 16;
+let GRID_SIZE = 16;
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  GRID_SIZE = parseInt(prompt("Enter size", "10"));
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
+  createRows();
+  createCells();
+});
 
 function createRows() {
   for (let i = 0; i < GRID_SIZE; i++) {
@@ -17,6 +27,7 @@ function createCells() {
       cell.className = "cell";
       cell.style.cssText = `width: calc((960px - 30px) / ${GRID_SIZE}); height: calc((960px - 30px) / ${GRID_SIZE});`;
       cell.addEventListener("mouseover", () => {
+        let colors = [];
         cell.style.backgroundColor = "pink";
       });
       allRows[i].append(cell);
